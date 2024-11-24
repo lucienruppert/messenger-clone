d702689ec3d73a50ece8430efc076059e389cbca
Ruppert Endre Lucien <121402304+lucienruppert@users.noreply.github.com>
2024-11-24 20:15:18 +0100

    connect to db

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MongoClient } from 'mongodb';
+
+const uri = 'mongodb+srv://luciendelmar:w77us2H1fyguW1gJ@cluster0.c0bviof.mongodb.net/?retryWrites=true&w=majority';
 
 async function bootstrap() {
+  const client = new MongoClient(uri);
+  await client.connect();
+  console.log('Connected to MongoDB Atlas database');
+
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(3000);
 }
 
 bootstrap();
