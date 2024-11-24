c3d45bbb7dfd7643c522fb75b6f0cfeb2ee5f377
Ruppert Endre Lucien <121402304+lucienruppert@users.noreply.github.com>
2024-11-24 19:19:52 +0100

    write backend connection test for hom component

--- frontend/src/app/pages/home/home.component.spec.ts
+++ frontend/src/app/pages/home/home.component.spec.ts
@@ -1,23 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let httpMock: HttpTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HttpClientModule, HomeComponent],
-    }).compileComponents();
+      imports: [HttpClientTestingModule, HomeComponent]
+    })
+    .compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should get message from backend', () => {
+    const message = 'Hello World!';
+    component.ngOnInit();
+    httpMock.expectOne('http://localhost:3000').flush(message);
+    expect(component.message).toBe(message);
   });
 });
