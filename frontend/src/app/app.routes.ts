33fa50138337055beb004f796b665e7f6e32f157
Ruppert Endre Lucien <121402304+lucienruppert@users.noreply.github.com>
2024-12-02 21:28:29 +0100

    create basic dashboard structure

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
-import { DashboardComponent } from './components/dashboard/dashboard/dashboard.component';
-import { ChatComponent } from './components/dashboard/chat/chat.component';
+import { DashboardComponent } from './components/dashboard-components/dashboard/dashboard.component';
+import { ChatComponent } from './components/dashboard-components/chat/chat.component';
+import { SidebarComponent } from './components/dashboard-components/sidebar/sidebar.component';
 import { LoginComponent } from './components/login/login.component';
 import { GeneralRouteGuardService } from './services/general-route-guard.service';
 import { AuthRedirectGuardService } from './services/auth-redirect-guard.service';
@@ -10,8 +11,22 @@ export const routes: Routes = [
     path: 'dashboard',
     component: DashboardComponent,
     canActivate: [GeneralRouteGuardService],
+    children: [
+      {
+        path: 'chat',
+        component: ChatComponent,
+      },
+      {
+        path: 'sidebar',
+        component: SidebarComponent,
+      },
+      {
+        path: '',
+        redirectTo: 'chat',
+        pathMatch: 'full',
+      },
+    ],
   },
-  { path: 'chat', component: ChatComponent },
   {
     path: '',
     component: LoginComponent,
