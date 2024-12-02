d27c3263eee061d4577517d5a982da727808cb5c
Ruppert Endre Lucien <121402304+lucienruppert@users.noreply.github.com>
2024-12-02 22:26:58 +0100

    try to fix connection indicator

--- frontend/src/app/components/dashboard-components/sidebar/sidebar.component.ts
+++ frontend/src/app/components/dashboard-components/sidebar/sidebar.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { NgStyle } from '@angular/common';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, interval, Subscription } from 'rxjs';
 import { CommonModule } from '@angular/common';
+import { AuthenticationService } from '../../../services/authentication.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -11,26 +12,28 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [NgStyle, CommonModule],
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit, OnDestroy {
   private isConnectedSubject = new BehaviorSubject<boolean>(true);
   public isConnected$ = this.isConnectedSubject.asObservable();
+  private checkInterval = 5000; // Check every 5 seconds
+  private subscription: Subscription | null = null;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private authentication: AuthenticationService) {}
 
   ngOnInit(): void {
-    this.http
-      .get('http://localhost:3000', { responseType: 'text' })
-      .subscribe({
-        next: (response) => {
-          this.isConnectedSubject.next(!!response);
-        },
-        error: (error: HttpErrorResponse) => {
-          this.isConnectedSubject.next(false);
-        },
-      });
+    this.subscription = interval(this.checkInterval).subscribe(() => {
+      this.http.get('http://localhost:3000').subscribe(
+        () => this.isConnectedSubject.next(true),
+        () => this.isConnectedSubject.next(false),
+      );
+    });
   }
 
-  public updateConnectionStatus(status: boolean): void {
-    this.isConnectedSubject.next(status);
+  public logout(): void {
+    this.authentication.logout();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
   }
 }
