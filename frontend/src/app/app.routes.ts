db5f397a389a5b65e24158a24dd4e19084011a8e
Ruppert Endre Lucien <121402304+lucienruppert@users.noreply.github.com>
2024-12-02 20:45:45 +0100

    Rename component to dashboard

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { DashboardComponent } from './pages/home/dashboard.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ChatComponent } from './components/chat/chat.component';
 import { LoginComponent } from './components/login/login.component';
 import { GeneralRouteGuardService } from './services/general-route-guard.service';
