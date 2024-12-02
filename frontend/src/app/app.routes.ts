a41d89220bbc5414f78081458b157c5975b93b8b
Ruppert Endre Lucien <121402304+lucienruppert@users.noreply.github.com>
2024-12-02 20:33:53 +0100

    relocate chat component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { ChatComponent } from './pages/chat/chat.component';
+import { ChatComponent } from './components/chat/chat.component';
 import { LoginComponent } from './components/login/login.component';
 import { GeneralRouteGuardService } from './services/general-route-guard.service';
 import { AuthRedirectGuardService } from './services/auth-redirect-guard.service';
