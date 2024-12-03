c5d7b6a5791b6d9c87551352ecb0979575f17ea9
Ruppert Endre Lucien <121402304+lucienruppert@users.noreply.github.com>
2024-12-03 12:33:22 +0100

    user both servers on the same port

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,14 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import WebSocket from 'ws';
+import * as http from 'http';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
-  console.log('Application is running on: http://localhost:3000');
 
-  const wss = new WebSocket.Server({ port: 50001 });
+  const server = http.createServer(app.getHttpAdapter().getInstance());
+  const wss = new WebSocket.Server({ server });
 
   wss.on('connection', (ws) => {
     console.log('Client connected');
@@ -21,6 +21,12 @@ async function bootstrap() {
     ws.on('close', () => {
       console.log('Client disconnected');
     });
+  });
+
+  server.listen(3000, () => {
+    console.log(
+      'Application and WebSocket server are running on: http://localhost:3000',
+    );
   });
 }
 
