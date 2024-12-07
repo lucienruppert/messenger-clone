e8d14ee8faf2016e37212a8dcf279366c73f6619
Ruppert Endre Lucien <121402304+lucienruppert@users.noreply.github.com>
2024-12-07 23:57:11 +0100

    update prod variables 2

--- frontend/src/app/environments/environment.prod.ts
+++ frontend/src/app/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  websocketUrl: 'ws://messenger-clone-production-a0eb.up.railway.app',
+  websocketUrl: 'wss://messenger-clone-production-a0eb.up.railway.app',
   BASE_URL: 'https://luciendelmar.com/backend/api/chat',
 };
