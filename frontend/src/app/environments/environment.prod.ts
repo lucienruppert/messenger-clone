e9fb2866d73c10faa3efa56fd169c60664dd1c71
Ruppert Endre Lucien <121402304+lucienruppert@users.noreply.github.com>
2024-12-09 06:50:54 +0100

    modify websocket URLs

--- frontend/src/app/environments/environment.prod.ts
+++ frontend/src/app/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   websocketUrl: 'wss://messenger-clone-production-a0eb.up.railway.app',
+  //websocketUrl: 'wss://backendi.hu/chat',
   BASE_URL: 'https://luciendelmar.com/backend/api/chat',
 };
--- frontend/src/app/environments/environment.ts
+++ frontend/src/app/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   websocketUrl: 'ws://localhost:3000',
+  //websocketUrl: 'wss://backendi.hu/chat',
   BASE_URL: 'https://luciendelmar.com/backend/api/chat',
 };
